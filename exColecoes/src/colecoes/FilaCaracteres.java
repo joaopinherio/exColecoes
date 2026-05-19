15f0172fbfbbfc2e7796a6a034d25e08f298d171
João Pinheiro <143855804+joaopinherio@users.noreply.github.com>
2026-05-19 11:37:40 +0000

    metodo apaga vogais classe QUEUE

--- exColecoes/src/colecoes/FilaCaracteres.java
+++ exColecoes/src/colecoes/FilaCaracteres.java
@@ -7,7 +7,15 @@ public class FilaCaracteres implements Collection{
     Queue<Character> fila;
 
     public FilaCaracteres(){
-        fila = new <>();
+        fila = new Queue <>();
+    }
+
+    public void onlyConsonants(String word){
+        for(int i = 0; i < word.length(); i++){
+            if(word.charAt(i) != 'a' || word.charAt(i) != 'e' || word.charAt(i) != 'i' || word.charAt(i) != 'o' || word.charAt(i) != 'u'){
+                fila.offer(word.charAt(i));
+            }
+        }
     }
 
 }
