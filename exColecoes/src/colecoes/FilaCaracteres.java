224bf9095e2e906fab5bdc9a793604fd1c4b4391
joaopinheiro <joaopdepaula@gmail.com>
2026-05-19 00:50:25 -0300

    ex2 setup

---
+++ exColecoes/src/colecoes/FilaCaracteres.java
@@ -0,0 +1,13 @@
+package colecoes;
+
+import java.util.Collection;
+import java.util.Queue;
+
+public class FilaCaracteres implements Collection{
+    Queue<Character> fila;
+
+    public FilaCaracteres(){
+        fila = new <>();
+    }
+
+}
