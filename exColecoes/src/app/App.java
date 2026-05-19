386ce4932aaf4d2487fd22676a79e3bbee7e31df
João Pinheiro <143855804+joaopinherio@users.noreply.github.com>
2026-05-19 15:53:30 +0000

    app optimization

--- exColecoes/src/app/App.java
+++ exColecoes/src/app/App.java
@@ -7,47 +7,47 @@ public class App {
     PilhaCaracteres pilha;
     FilaCaracteres fila;
     ConjuntoCaracteres conjunto;
+    DicionarioCaracteres dicionario;
 
     public App() {
         pilha = new PilhaCaracteres();
         fila = new FilaCaracteres();
         conjunto = new ConjuntoCaracteres();
+        dicionario = new DicionarioCaracteres();
     }
 
     public void executar() {
-        ex1();
+        String palavra = "Teste";
 
-        ex2();
+        ex1(palavra);
 
-        ex3();
+        ex2(palavra);
+
+        ex3(palavra);
+
+        ex4(palavra);
 
     }
 
-    public void ex1() {
-        String palavra = "Teste";
-
+    public void ex1(String palavra) {
         pilha.reverseString(palavra);
 
-        System.out.println("reverse:");
+        System.out.println("Palavra ao Contrario:");
         pilha.printStack();
         System.out.println();
     }
 
-    public void ex2() {
-        String palavra = "Teste";
-
+    public void ex2(String palavra) {
         fila.onlyConsonants(palavra);
 
-        System.out.println("sem vogais:");
+        System.out.println("Sem vogais:");
         fila.printQueue();
 
         System.out.println();
     }
 
-    public void ex3() {
-        String palavra = "Teste";
-
-        System.out.println("sem repeticoes:");
+    public void ex3(String palavra) {
+        System.out.println("Sem repeticoes:");
 
         conjunto.addOneTime(palavra);
         System.out.println();
@@ -55,6 +55,11 @@ public class App {
         conjunto.printSet();
 
         System.out.println();
+    }
+
+    public void ex4(String palavra){
+        System.out.println("Letra/Ocorrencias");
+        
     }
 
 }
